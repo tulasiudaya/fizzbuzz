b710f8abc8ad3ad8e2f3aaf82b50a6edb7c7f5e8
tulasiudaya <Udaya.tulasi@dal.ca>
2019-05-23 07:48:39 -0700

    final code

--- fizz.py
+++ fizz.py
@@ -1,13 +1,13 @@
 def fizz():
     j=101
-    for i in range(101)
-        if i%3 == 0 and i%5 == 0
-	    print ('fizzbuzz\n')
-	elif i%3 == 0
-	    print ('fizz\n')
-	elif i%5 == 0
-	    print ('buzz\n')
-	else 
-	    print (i'\n')
+    for i in range(101):
+        if i%3 == 0 and i%5 == 0:
+	    print('fizzbuzz'\n)
+	elif i%3 == 0:
+	    print('fizz'\n)
+	elif i%5 == 0:
+	    print('buzz'\n)
+	else:
+	    print(i\n)
 
 fizz()
